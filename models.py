494859def2fbe923a0bdeef9aa314b75a8601475
bel4vskiy <stas.belavskiy@yandex.com>
2024-09-19 21:00:34 +0300

    update models.py

--- models.py
+++ models.py
@@ -17,5 +17,23 @@ def checkUser(login, psw):
         return False
     return True
 
+def createLot(userID, name_lot, lot_description, min_price_lot, step, price_for_buy):
+    cursor.execute(f"INSERT INTO Active_lots(user_id, name_lot, lot_description, min_price_lot, step, price_for_buy) VALUES({userID}, '{name_lot}', '{lot_description}', {min_price_lot}, {step}, {price_for_buy})")
+    con.commit()
+    return True
+
+def addBid(id_lot, userID, amount, created_at):
+    cursor.execute(f"INSERT INTO Bids_table(id_lot, userID, amount, created_at) VALUES({id_lot}, {userID}, {amount}, {created_at})")
+    con.commit()
+    return True
+
+def addLotHistory(id_lot, status, current_price, buyer):
+    cursor.execute(f"INSERT INTO Lots_history(id_lot, status, current_price, buyer) VALUES({id_lot}, '{status}', {current_price}, {buyer})")
+    con.commit()
+    return True
+
+def getLots():
+    cursor.execute(f"SELECT * FROM Active_lots WHERE NOT EXISTS (SELECT * FROM Lots_history WHERE Lots_history.id_lot = Active_lots.id_lot AND Lots_history.status = 'Не завершен')")
+
 # createUser('Stanislaw', '1234')
 # checkUser('Stanislaw', '')
